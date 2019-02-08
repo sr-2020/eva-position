b29fe68b49006e9d38eae8e4882818b6cba2b6f6
Dmitry Gurkalov <gurkalov.dev@gmail.com>
2019-02-08 09:44:38 +0300

    Fix Beacon model

--- src/app/Beacon.php
+++ src/app/Beacon.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 
 /**
- * @OA\Schema(schema="BeaconSignal", required={"ssid", "bbsid", "level"},
+ * @OA\Schema(schema="BeaconSignal", required={"ssid", "bssid", "level"},
  *    @OA\Property(property="ssid", format="string", type="string", example="beacon1"),
  *    @OA\Property(property="bssid", format="string", type="string", example="b0:0a:95:9d:00:0a"),
  *    @OA\Property(property="level", format="int64", type="integer", example=-50),
@@ -21,7 +21,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 
 /**
- * @OA\Schema(schema="Beacon", required={"ssid", "bbsid", "level"},
+ * @OA\Schema(schema="Beacon", required={"ssid", "bssid"},
  *    @OA\Property(property="id", format="int64", type="integer", example=1),
  *    @OA\Property(property="label", format="string", type="string", example="Hall Room"),
  *    @OA\Property(property="ssid", format="string", type="string", example="beacon1"),
@@ -49,5 +49,16 @@ class Beacon extends Model
     public function users()
     {
         return $this->hasMany('App\User');
+    }
+
+    /**
+     * Get the beacon's label.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getLabelAttribute($value)
+    {
+        return (string)$value;
     }
 }
