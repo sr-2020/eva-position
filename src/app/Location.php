75a286e4e19afd6bb1a3ec7c8d1b922a89947d60
Dmitry Gurkalov <gurkalov.dev@gmail.com>
2019-03-03 21:52:35 +0300

    Fix openapi for Location model

--- src/app/Location.php
+++ src/app/Location.php
@@ -4,6 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(schema="NewLocation", required={"label"},
+ *     @OA\Property(property="label", format="string", type="string", default="Hall Room", example="Hall Room"),
+ * )
+ */
+
+/**
+ * @OA\Schema(schema="Location", required={"id", "label"},
+ *    @OA\Property(property="id", format="int64", type="integer", example=1),
+ *    @OA\Property(property="label", format="string", type="string", example="Hall Room")
+ * )
+ */
+
 class Location extends Model
 {
     protected $fillable = [
