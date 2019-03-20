02afe9a8a40c2bd7071785790404ec013413d7b3
Dmitry Gurkalov <gurkalov.dev@gmail.com>
2019-03-20 20:41:54 +0300

    Fix /api/documentation for balancer

--- src/app/Support/helpers.php
+++ src/app/Support/helpers.php
@@ -13,3 +13,25 @@ if (! function_exists('now')) {
         return Carbon::now();
     }
 }
+
+if (! function_exists('app_swagger_lume_asset')) {
+    /**
+     * Returns asset from swagger-ui composer package.
+     *
+     * @param $asset string
+     *
+     * @return string
+     * @throws \SwaggerLume\Exceptions\SwaggerLumeException
+     */
+    function app_swagger_lume_asset($asset)
+    {
+        $file = swagger_ui_dist_path($asset);
+
+        if (! file_exists($file)) {
+            throw new SwaggerLumeException(sprintf('Requested L5 Swagger asset file (%s) does not exists', $asset));
+        }
+
+        $directory = config('swagger-lume.routes.assets');
+        return $directory . '/' . $asset . '?v=' . md5($file);
+    }
+}
