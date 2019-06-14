a90794814559ee9257a1a08da9577302678a5a0a
Gurkalov Dmitry <gurkalov.dev@gmail.com>
2019-06-15 00:16:59 +0200

    Overload env variables

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -2,9 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
+try {
+    (Dotenv\Dotenv::create(__DIR__ . '/../'))->overload();
+} catch (Dotenv\Exception\InvalidPathException $e) {
+    //
+}
 
 /*
 |--------------------------------------------------------------------------
