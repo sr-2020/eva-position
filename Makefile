60530f57f2a0350c9683dbad8fa65be77b5ba7e1
Gurkalov Dmitry <gurkalov.dev@gmail.com>
2019-06-19 22:20:08 +0200

    Add draft Kubernetes

--- Makefile
+++ Makefile
@@ -35,7 +35,7 @@ up:
 	docker-compose up -d
 
 dev:
-	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
+	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 
 down:
 	docker-compose down
--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 try {
-    (Dotenv\Dotenv::create(__DIR__ . '/../'))->overload();
+    (Dotenv\Dotenv::create(__DIR__ . '/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
